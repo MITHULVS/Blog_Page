258e9c21abe3844026bafb440c550fbe17acda97
MITHUL V S <vsmithul2007@gmail.com>
2025-11-28 18:02:40 +0530

    Add BlogPost model with fields for title, author, and content

---
+++ models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
